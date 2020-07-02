1efa9cd0d0950dc2ef0aac4c009f338e6e6ee42a
serginho6x <serginho_6x@hotmail.com>
2020-07-02 11:11:22 -0400

    refactor

--- src/sendToArduino/sendToArduino.java
+++ src/sendToArduino/sendToArduino.java
@@ -4,8 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Scanner;
 
 import javax.swing.JButton;
@@ -52,29 +50,15 @@ public class sendToArduino {
 			@Override public void run() {
 				// wait after connecting, so the bootloader can finish
 				try {Thread.sleep(100); } catch(Exception e) {}
-
 				// enter an infinite loop that sends text to the arduino
 				PrintWriter output = new PrintWriter(chosenPort.getOutputStream());
 				String value;
-				Scanner scanner = new Scanner(System.in);
-				//chosenPort.setBaudRate(9600);
-				while(true) {
-					value = scanner.next();
-					//output.print(new SimpleDateFormat("hh:mm:ss a     MMMMMMM dd, yyyy").format(new Date()));
-					output.print(value);
-					output.flush();
-					////
-					//Scanner data = new Scanner(chosenPort.getInputStream());
-					//String value = "";
-					//System.out.println(chosenPort.getBaudRate());
-					
-					//while(data.hasNextLine()){
-					//	try{value = (data.nextLine());}catch(Exception e){System.out.println(e);}
-						//slider.setValue(value);
-					//	System.out.println(value);}
-					////
-					//try {Thread.sleep(100); } catch(Exception e) {}
-					
+				try (Scanner scanner = new Scanner(System.in)) {
+					while(true) {
+						value = scanner.next();
+						output.print(value);
+						output.flush();
+					}
 				}
 			}
 		};
@@ -83,8 +67,6 @@ public class sendToArduino {
 	
 	public static void main(String[] args) {
 		JFrame window = setJFrame();
-		
-		
 		// create a drop-down box and connect button, then place them at the top of the window
 		JComboBox<String> portList = new JComboBox<String>();
 		JButton connectButton = new JButton("Connect");
@@ -104,8 +86,6 @@ public class sendToArduino {
 				extracted(portList, connectButton);
 			}
 		});
-		
-		// show the window
 		window.setVisible(true);
 	}
 
